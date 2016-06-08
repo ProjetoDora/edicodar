0ebe50a957afb174ce7e21d6668da994c274f38a
anikarni <anikarni@gmail.com>
2016-06-08 20:30:35 -0300

    #1 Remove ponto e virgula

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const electron = require('electron')
+const { app, BrowserWindow } = electron
 
-let win;
+let win
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadURL(`file://${__dirname}/index.html`);
+  win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadURL(`file://${__dirname}/index.html`)
 
   win.webContents.on('did-finish-load', function() {
-    win.webContents.executeJavaScript("CodeMirror(document.body, {mode: 'javascript'});");
-  });
+    win.webContents.executeJavaScript("CodeMirror(document.body, {mode: 'javascript'});")
+  })
 
-  win.focus();
-  win.on('closed', () => win = null);
+  win.focus()
+  win.on('closed', () => win = null)
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
