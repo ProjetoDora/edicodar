77d677302a927e327fa70adcc00b8ad106ec3c0c
anikarni <anikarni@gmail.com>
2016-06-08 22:17:01 -0300

    #2 Adiciona react-codemirror

--- editor.js
+++ editor.js
@@ -1,11 +1,34 @@
-const CodeMirror = require('codemirror')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Codemirror = require('react-codemirror')
 
-document.addEventListener('DOMContentLoaded', function () {
-  CodeMirror(document.body, {
-    theme: 'material',
-    lineNumbers: true,
-    value: 'Bem vindo ao Projeto Dora!',
-    autofocus: true,
-    mode: 'javascript'
-  })
+const App = React.createClass({
+	displayName: 'App',
+
+	getInitialState: function getInitialState() {
+		return {
+			code: 'Bem vindo ao Projeto Dora!'
+		}
+	},
+	updateCode: function updateCode(newCode) {
+		this.setState({
+			code: newCode
+		})
+	},
+	render: function render() {
+		const options = {
+      theme: 'material',
+      lineNumbers: true,
+      autofocus: true,
+      mode: 'javascript'
+		}
+		return React.createElement(
+      Codemirror, {
+        value: this.state.code,
+        onChange: this.updateCode,
+        options: options
+      })
+	}
 })
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'))
