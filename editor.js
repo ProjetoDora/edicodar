80caddae3cd814e741f0fee61fcc5ab8659a6ac2
anikarni <anikarni@gmail.com>
2016-06-08 22:01:42 -0300

    #2 Adiciona tema para codemirror

--- editor.js
+++ editor.js
@@ -1,7 +1,9 @@
 const CodeMirror = require('codemirror')
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   CodeMirror(document.body, {
+    theme: 'material',
+    lineNumbers: true,
     value: 'Bem vindo ao Projeto Dora!',
     autofocus: true,
     mode: 'javascript'
