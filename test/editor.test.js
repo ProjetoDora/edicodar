89818a13cab1ac614c36fe9c236e9a84be9a40f7
anikarni <anikarni@gmail.com>
2016-06-15 20:01:15 -0300

    #2 Remove console.log

--- test/editor.test.js
+++ test/editor.test.js
@@ -6,7 +6,6 @@ const Editor = require('../src/editor')
 describe('Editor', () => {
   it('renders CodeMirror', () => {
     const editorComponent = shallow(React.createElement(Editor, null))
-    console.log(editorComponent.node.type.displayName)
     expect(editorComponent.node.type.displayName).to.equal('CodeMirror')
   })
 })
